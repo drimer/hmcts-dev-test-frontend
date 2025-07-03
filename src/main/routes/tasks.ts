53e0a3c90dd46d6c56c71e3f3c78a269a9f9cf1f
Alberto Aguilera <drimer.aaj@gmail.com>
2025-07-03 16:14:59 +0100

    chore: move task-related routes to separate file

---
+++ src/main/routes/tasks.ts
@@ -0,0 +1,65 @@
+import { Application } from 'express';
+import axios from 'axios';
+
+
+export default function (app: Application): void {
+  app.get('/tasks/create', async (req, res) => {
+    res.render('create-task');
+  })
+
+  app.post('/tasks/create', async (req, res) => {
+    try {
+      const apiUrl = `http://localhost:4000/tasks`;
+      const apiTasksPostRequestDto = {
+        "title": req.body.title,
+        "description": req.body.description,
+        "status": req.body.status
+      }
+      await axios.post(apiUrl, apiTasksPostRequestDto);
+
+      res.redirect(`/`);
+    } catch (error) {
+      res.render('create-task', {
+        "title": req.body.title,
+        "description": req.body.description,
+        "status": req.body.status,
+        "errors": error.response.data
+      });
+    }
+  })
+
+  app.get('/tasks/:id', async (req, res) => {
+    try {
+      const response = await axios.get(`http://localhost:4000/tasks/${req.params.id}`);
+      res.render('task', { "task": response.data });
+    } catch (error) {
+      console.error('Error making request:', error);
+      res.render('home', {});
+    }
+  });
+
+  app.post('/tasks/:id', async (req, res) => {
+    try {
+      const apiUrl = `http://localhost:4000/tasks/${req.params.id}`;
+      const apiTasksPostRequestDto = { status : req.body.status}
+      await axios.patch(apiUrl, apiTasksPostRequestDto);
+
+      res.redirect(`/`);
+    } catch (error) {
+        const response = await axios.get(`http://localhost:4000/tasks/${req.params.id}`);
+        res.render('task', { "task": response.data, "errors": error.response.data });
+    }
+  });
+
+  app.get('/tasks/:id/delete', async (req, res) => {
+    try {
+      const apiUrl = `http://localhost:4000/tasks/${req.params.id}`;
+      await axios.delete(apiUrl);
+
+      res.redirect(`/`);
+    } catch (error) {
+      const response = await axios.get('http://localhost:4000/tasks');
+      res.render('home', { "tasks": response.data, "errors": error.response.data });
+    }
+  })
+}
